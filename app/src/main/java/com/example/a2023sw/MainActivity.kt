cf662ce4ae4bf73c73eff9f415e727f1b8676cfa
ya012 <dbsdk301@gmial.com>
2023-11-01 12:36:13 +0900

    six commit

--- app/src/main/java/com/example/a2023sw/MainActivity.kt
+++ app/src/main/java/com/example/a2023sw/MainActivity.kt
@@ -11,9 +11,7 @@ import com.example.a2023sw.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
     private lateinit var binding: ActivityMainBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
